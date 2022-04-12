4ab220954c39000deae6211836433cfa93b62a23
johandry8919 <rojas.rojas440@gmail.com>
2022-04-12 07:26:41 -0400

    Registro_usuario

--- application/views/usuarios/NuevoRegistro.php
+++ application/views/usuarios/NuevoRegistro.php
@@ -34,117 +34,160 @@
 
 </head>
 
-<body>
+<body class="app sidebar-mini ltr">
 
-    <div class="card-body">
+    <div class="body">
+            <div class="page">
 
-        <div class="container">
-            <div class="wrap-login100 p-6">
-                <form id="sign_up" action="<?php echo base_url(); ?>Cusuarios/registro" method="POST">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <label class="form-label">Persona</label>
-                            <div class="form-group">
-                                <select class="form-control" id="nac" name="nac" required>
-                                    <option value="" selected="">Seleccione</option>
-                                    <option value="V">Venezolano(a)</option>
-                                    <option value="E">Extranjero(a)</option>
-                                </select>
-                                <?php echo form_error('nac'); ?>
-                            </div>
-
-                            <label class="form-label">Correo</label>
-                            <div class="form-group">
-                                <div class=" validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
-                                    <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
-                                        <i class="zmdi zmdi-email" aria-hidden="true"></i>
-                                    </a>
-                                    <input class="input100 border-start-0 ms-0 form-control" type="email" maxlength="100" id="email" name="email" value="<?php echo set_value('email'); ?>" placeholder="Correo" required>
-                                    <?php echo form_error('email'); ?>
-                                </div>
-
-
-                            </div>
-
-
-                            <label class="form-label">Contraseña</label>
-                            <div class="form-group">
-                                <div class=" validate-input input-group" id="Password-toggle">
-                                    <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
-                                        <i class="zmdi zmdi-eye" aria-hidden="true"></i>
-                                    </a>
-                                    <input class="input100 border-start-0 ms-0 form-control" type="password" id="password" maxlength="16" name="password" value="<?php echo set_value('password'); ?>" placeholder="Contraseña" required>
-                                </div>
-
-
-                            </div>
-
-
-
-
+                <div class="container  mx-auto justify-content-center ">
+                <div class="wrap-login100 p-6">
+                <div class="col col-login mx-auto mt-7">
+                        <div class="text-center">
+                        <a href="javascript:void(0);"><img src="<?php echo base_url(); ?>img/logo-nuevo-chamba-250-79.png" class="img-fluid" alt="Responsive image"></a>
                         </div>
-
-
-                        <div class="col-lg-6">
-                            <div class="form-group">
-                                <label class="form-label">Cedula</label>
-                                <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
-                                    <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
-                                        <i class="mdi mdi-account" aria-hidden="true"></i>
-                                    </a>
-                                    <input class="input100 border-start-0 ms-0 form-control" type="text" id="cedula" maxlength="8" name="cedula" value="<?php echo set_value('cedula'); ?>" placeholder="Cédula" required autofocus>
-                                    <?php echo form_error('cedula'); ?>
+                    </div>
+                  
+                    <form id="sign_up" action="<?php echo base_url(); ?>Cusuarios/registro" method="POST" class="login100-form">
+                        <div class="row ">
+                            <div class="col-lg-6">
+                            <!-- <ul class="nav panel-tabs">
+                                            <li class="mx-0"><a href="#tab5" class="active" data-bs-toggle="tab">Persona</a></li>
+                                           
+                                        </ul> -->
+                                <div class="form-group">
+                                    <select class="form-control" id="nac" name="nac" required>
+                                        <option value="" selected="">Seleccione</option>
+                                        <option value="V">Venezolano(a)</option>
+                                        <option value="E">Extranjero(a)</option>
+                                    </select>
+                                    <?php echo form_error('nac'); ?>
                                 </div>
-
-                            </div>
-
-
-                            <div class="form-group">
-                                <label class="form-label">Repetir Correo</label>
-                                <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
-                                    <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
-                                        <i class="zmdi zmdi-email" aria-hidden="true"></i>
-                                    </a>
-                                    <input class="input100 border-start-0 ms-0 form-control" type="email" id="passwordr" maxlength="100" id="emailr" name="emailr" value="<?php echo set_value('emailr'); ?>" placeholder="Repetir Correo" required>
-                                    <?php echo form_error('emailr'); ?>
+    
+                                <!-- <label class="form-label">Correo</label> -->
+                                <div class="form-group">
+                                    <div class=" validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
+                                        <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                            <i class="zmdi zmdi-email" aria-hidden="true"></i>
+                                        </a>
+                                        <input class="input100 border-start-0 ms-0 form-control" type="email" maxlength="100" id="email" name="email" value="<?php echo set_value('email'); ?>" placeholder="Correo" required>
+                                        <?php echo form_error('email'); ?>
+                                    </div>
+    
+    
                                 </div>
-
-
-                                <label class="form-label">Repetir Contraseña</label>
+    
+    
+                                <!-- <label class="form-label">Contraseña</label> -->
                                 <div class="form-group">
                                     <div class=" validate-input input-group" id="Password-toggle">
                                         <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
                                             <i class="zmdi zmdi-eye" aria-hidden="true"></i>
                                         </a>
-                                        <input class="input100 border-start-0 ms-0 form-control" type="password" id="passwordr" maxlength="16" name="passwordr" value="<?php echo set_value('passwordr'); ?>" placeholder="Repetir Contraseña" required>
-                                        <?php echo form_error('passwordr'); ?>
+                                        <input class="input100 border-start-0 ms-0 form-control" type="password" id="password" maxlength="16" name="password" value="<?php echo set_value('password'); ?>" placeholder="Contraseña" required>
                                     </div>
-
-
+    
+    
+                                </div>
+    
+    
+    
+    
+                            </div>
+    
+    
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <!-- <label class="form-label">Cedula</label> -->
+                                    <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
+                                        <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                            <i class="mdi mdi-account" aria-hidden="true"></i>
+                                        </a>
+                                        <input class="input100 border-start-0 ms-0 form-control" type="text" id="cedula" maxlength="8" name="cedula" value="<?php echo set_value('cedula'); ?>" placeholder="Cédula" required autofocus>
+                                        <?php echo form_error('cedula'); ?>
+                                    </div>
+    
+                                </div>
+    
+    
+                                <div class="form-group">
+                                    <!-- <label class="form-label">Repetir Correo</label> -->
+                                    <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
+                                        <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                            <i class="zmdi zmdi-email" aria-hidden="true"></i>
+                                        </a>
+                                        <input class="input100 border-start-0 ms-0 form-control" type="email" id="passwordr" maxlength="100" id="emailr" name="emailr" value="<?php echo set_value('emailr'); ?>" placeholder="Repetir Correo" required>
+                                        <?php echo form_error('emailr'); ?>
+                                    </div>
+    
+    
+                                    <!-- <label class="form-label">Repetir Contraseña</label> -->
+                                    <div class="form-group">
+                                        <div class=" validate-input input-group" id="Password-toggle">
+                                            <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                                <i class="zmdi zmdi-eye" aria-hidden="true"></i>
+                                            </a>
+                                            <input class="input100 border-start-0 ms-0 form-control" type="password" id="passwordr" maxlength="16" name="passwordr" value="<?php echo set_value('passwordr'); ?>" placeholder="Repetir Contraseña" required>
+                                            <?php echo form_error('passwordr'); ?>
+                                        </div>
+    
+    
+                                    </div>
+                                </div>
+    
+                            </div>
+                            <?php if($this->session->flashdata('mensajeerror')){ ?>
+                            <div class="row">
+                              <div class="col-md-12">
+                                  <div class="alert alert-danger"> <?php echo $this->session->flashdata('mensajeerror'); ?></div>
+                              </div>
+                            </div>
+                      <?php }?>
+    
+                      <?php if($this->session->flashdata('mensajeexito')){ ?>
+                            <div class="row">
+                              <div class="col-md-12">
+                                  <div class="alert alert-success"> <?php echo $this->session->flashdata('mensajeexito'); ?></div>
+                              </div>
+                            </div>
+                      <?php }?>
+                            <div class="row" id="loader_romel" style="display: none;">
+                                <div class="col-md-4 col-md-offset-5">
+                                    <div class="preloader pl-size-xl">
+                                        <div class="spinner-layer">
+                                            <div class="circle-clipper left">
+                                                <div class="circle"></div>
+                                            </div>
+                                            <div class="circle-clipper right">
+                                                <div class="circle"></div>
+                                            </div>
+                                        </div>
+                                    </div>                            
                                 </div>
                             </div>
-
+                          
+    
+    
+                            <!-- <button class="container-login100- form-btn"  >REGISTRAR</button>
+                             -->
+                             <div class="container-login100-form-btn">
+                                    <button id="boton" type="botton"  class="login100-form-btn btn-primary">
+                                            Register
+                                        </button>
+                                </div>
+                            <div class="m-t-25 m-b--5 p-2 align-center text-center">
+                            <a class="text-center" href="<?php echo base_url();?>iniciosesion">Ya estoy registrado</a>
+    
+                           
                         </div>
-
-
-                        <div id="boton" class="container-login100-form-btn">
-                            <a href="index.html" class="login100-form-btn btn-primary">
-                                REGISTRAR
-                            </a>
-                        </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
 
-
     </div>
 
 
     </div>
-    <!-- End PAGE -->
-
-
-
 
     <!-- PAGE -->
     <!-- BACKGROUND-IMAGE CLOSED -->
