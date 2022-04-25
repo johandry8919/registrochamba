3bf93ebfd8f04b0e8f0ee4b52715da074e530e2f
jhonatan <jhonatanrojasmd@gmail.com>
2022-04-25 01:34:41 -0400

    confi

--- application/views/principal/chambistas/sql pendientes.sql
+++ application/views/principal/chambistas/sql pendientes.sql
@@ -4,4 +4,4 @@ ALTER TABLE tbl_usuarios_personales ADD COLUMN id_profesion_oficio integer DEFAU
 ALTER TABLE tbl_usuarios_personales ADD COLUMN edad bigint DEFAULT 0;
 
 ALTER TABLE tbl_usuarios_personales ADD COLUMN id_movimiento_religioso integer DEFAULT 0;
-ALTER TABLE tbl_usuarios_personales ADD COLUMN id_movimiento_movimientos socialies integer DEFAULT 0;
+ALTER TABLE tbl_usuarios_personales ADD COLUMN id_movimiento_socialies integer DEFAULT 0;
