c85e39efe4a6ba34ba5829fbede93ac152987636
jhonatan <jhonatanrojasmd@gmail.com>
2022-07-21 17:04:22 -0400

    ofertas

--- application/config/recursos.php
+++ application/config/recursos.php
@@ -28,12 +28,12 @@
     $config['recursos']['listar_usuario_admin_js'] = "assets/js/admin/admin_registrar_usuario.js?v=1.0.6";
     $config['recursos']['buscar_chambista_js'] = "assets/js/admin/buscar_chambista.js?v=1.0.6";
     $config['recursos']['editar_chambista_js'] = "assets/js/admin/editar_chambista.js?v=1.0.6";
-    $config['recursos']['admin_nueva_oferta_js'] = "assets/js/admin/admin_nueva_oferta.js?v=1.0.6";
-    $config['recursos']['admin_nueva_oferta_uner_js'] = "assets/js/admin/admin_nueva_oferta_uner.js?v=1.0.6";
+    $config['recursos']['admin_nueva_oferta_js'] = "assets/js/admin/admin_nueva_oferta.js?v=1.0.7";
+    $config['recursos']['admin_nueva_oferta_uner_js'] = "assets/js/admin/admin_nueva_oferta_uner.js?v=1.0.7";
     $config['recursos']['editar_formacion_cade_js'] = "assets/js/admin/editar_formacion_cade.js?v=1.0.6";
-    $config['recursos']['listar_oferta_js'] = "assets/js/admin/listar_oferta.js?v=1.0.6";
-    $config['recursos']['ver_oferta_js'] = "assets/js/admin/ver_oferta.js?v=1.0.6";
-    $config['recursos']['ver_oferta_universidad_js'] = "assets/js/admin/ver_oferta_universidad.js?v=1.0.6";
+    $config['recursos']['listar_oferta_js'] = "assets/js/admin/listar_oferta.js?v=1.0.7";
+    $config['recursos']['ver_oferta_js'] = "assets/js/admin/ver_oferta.js?v=1.0.7";
+    $config['recursos']['ver_oferta_universidad_js'] = "assets/js/admin/ver_oferta_universidad.js?v=1.0.7";
     $config['recursos']['roles_js'] = "assets/js/admin/roles.js?v=1.0.6";
     $config['recursos']['rolesUsuarios_js'] = "assets/js/admin/rolesUsuarios.js?v=1.0.6";
     $config['recursos']['rol_EditEstructurajs_js'] = "assets/js/admin/rol_EditEstructurajs.js?v=1.0.6";
